d0538593abb3e0e40bb2e4bdf996c6b10dc10453
AlejandroAP98 <aalvarezpa@unal.edu.co>
2025-03-27 10:30:20 -0500

    frima de autor

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+// 
+// Autor: Alejandro Álvarez Patiño
+// https://github.com/AlejandroAP98
+// Fecha: 2025-27-03
+// Descrpción: Aplicación andorid, complemento para el sistema de inventairo de productos.
+// Proyecto: ScannerStock
+
+
 import "react-native-gesture-handler";
 import { registerRootComponent } from 'expo';
-
 import App from './App.tsx';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
